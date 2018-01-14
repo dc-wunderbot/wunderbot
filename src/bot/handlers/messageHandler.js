119691f510f26d70de53ab88d85c4dc05ba42af0
ineyy <vfioox@gmail.com>
2018-01-14 21:27:39 +0100

    added command loading error handling; added a constant for failure timeouts; increasing overall code quality

--- src/bot/handlers/messageHandler.js
+++ src/bot/handlers/messageHandler.js
@@ -4,6 +4,8 @@ import cfg from "config";
 import fs from "fs";
 import path from "path";
 
+const DEFAULT_FAILURE_TIMEOUT = 5000;
+
 class MessageHandler {
     /**
      * Handles all the messages from discord and sends to correct subcommand.
@@ -27,29 +29,40 @@ class MessageHandler {
             if (err) console.error(err);
 
             const jsfiles = files.filter(f => f.split(".").pop() === "js");
+
             if (jsfiles.length <= 0) {
                 console.log("No commands to load!");
                 return null;
             }
 
-            console.log(`Loading ${jsfiles.length} commands!`);
+            console.log(`Loading ${jsfiles.length} commands...`);
 
-            jsfiles.forEach((f, i) => {
-                import(path.join(path.join(__dirname, `../cmds/${f}`))).then(p => {
-                    let props = new p.default(Discord, this.bot, this.db, this.db.getRawRethink()); // eslint-disable-line
-                    this.commands.set(props.info.bCommand, props);
-                });
+            jsfiles.forEach((fileName, i) => {
+                import(path.join(__dirname, `../cmds/${fileName}`))
+                    .then(commandModule => {
+                        let props = new commandModule.default(Discord, this.bot, this.db, this.db.getRawRethink()); // eslint-disable-line
+                        this.commands.set(props.info.bCommand, props);
+                        console.log(`Successfuly loaded ${this.trigger}${props.info.bCommand}`);
+                    })
+                    .catch(error => {
+                        console.log(`Command located in ${fileName} has failed to load with the error:`);
+                        console.log(error);
+                    })
             });
         });
     }
 
     async handleMsg(msg) {
         if (msg.author.bot) return;
+
         const msgArray = msg.content.split(/\s+/g);
         const command = msgArray[0];
         const args = msgArray.slice(1);
+
         if (!command.startsWith(this.trigger)) return;
+
         const cmd = this.commands.get(command.slice(this.trigger.length));
+
         if (cmd && msg.channel.type === "dm" && !cmd.info.pm) {
             this.noPm(msg);
         } else if (cmd && msg.channel.type === "dm") {
@@ -69,19 +82,19 @@ class MessageHandler {
     async notEnabled(msg) {
         msg.react("❌");
         const m = await msg.channel.send("❌ That command is not enabled on this server.");
-        m.delete(5000);
+        m.delete(DEFAULT_FAILURE_TIMEOUT);
     }
 
     async noPerm(msg) {
         msg.react("❌");
         const m = await msg.channel.send("❌ You do not have the rank needed to use this command.");
-        m.delete(5000);
+        m.delete(DEFAULT_FAILURE_TIMEOUT);
     }
 
     async noPm(msg) {
         msg.react("❌");
         const m = await msg.channel.send("❌ This command can´t be called from PMs.");
-        m.delete(5000);
+        m.delete(DEFAULT_FAILURE_TIMEOUT);
     }
 }
 
